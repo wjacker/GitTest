fb1fe73723626d17c362bec4f74ffb99b9a57ce5
王杰 <wangjie.cosmo@haier.com>
2018-05-21 14:51:56 +0800

    renamed class name

---
+++ TestAViewController2.h
@@ -0,0 +1,13 @@
+//
+//  TestAViewController.h
+//  HaierDiy
+//
+//  Created by Jack on 16/05/2018.
+//  Copyright © 2018 haier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestAViewController : UIViewController
+
+@end
